ecfadd4e01725584f11e80e19c3836f868e00755
NahuelAlejandro <nahuelmoreno283@gamil.com>
2024-02-24 21:57:47 -0300

    se agrega filtro de busqueda y por genero del sprint 2 JavaScript

--- scripts/movies.js
+++ scripts/movies.js
@@ -1,3 +1,4 @@
+// MENU MOBILE
 const openMenu = document.getElementById("menu");
 const drop = document.getElementById("dropMenu");
 const close = document.getElementById("close");
@@ -15,16 +16,18 @@ openMenu.addEventListener("click",dropMenu );
 close.addEventListener("click", dropMenu)
 
 
+
+// MAIN CONTENT
 const movieContainer = document.getElementById("movieContainer");
 
 function createCards(movie) {
     
 
     let content =document.createElement("article");
-    content.className = `bg-[#6d38E0] shadow-lg shadow-[#6d38E0]/50 hover:shadow-xl hover:shadow-[#6d38E0]/50 cursor-pointer rounded-lg lg:rounded-t-lg overflow-hidden flex flex-col gap-2 items-center  h-80 w-[45%] lg:w-[22%] lg:h-[450px] `
+    content.className = `bg-[#6d38E0] shadow-lg shadow-[#6d38E0]/50 hover:shadow-xl hover:shadow-[#6d38E0]/50 cursor-pointer rounded-lg lg:rounded-t-lg overflow-hidden flex flex-col gap-2 items-center  h-80 w-[90%] md:w-[45%] lg:w-[28%] lg:h-[500px] `
     content.innerHTML = `
         <img class="object-cover h-[80%] lg:h-[55%] lg:w-full h-full " src="${movie.image}" alt="">
-        <div class="w-[90%] lg:flex lg:flex-col lg:gap-2 pb-4 lg:static lg:h-[40%]">
+        <div class="w-[90%] lg:flex lg:flex-col lg:justify-around lg:gap-2  lg:static lg:h-[40%]">
             <h3 class="font-medium text-center lg:text-lg  text-white lg:py-2 ">${movie.title}</h3>
             <span class=" hidden lg:block lg:text-[#d2ccff] ">${movie.tagline}</span>
             <p class="hidden lg:block lg:text-[#d2ccff] lg:text-sm lg:py-2 lg:truncate">${movie.overview}</p>
@@ -32,14 +35,116 @@ function createCards(movie) {
     return content;
 }
 
-function printCards (movies, element){
+function printCards (moviesList, element){
     let fragment = document.createDocumentFragment();
 
-    for (const movie of movies) {
-        fragment.append( createCards(movie) ) ;
+    for (const movie of moviesList) {
+        fragment.append( createCards(movie)) ;
          
         }
-         element.appendChild(fragment);
+         element.append(fragment);
 }
 
-printCards(movies,movieContainer )
+printCards(movies, movieContainer)
+
+
+// FILTER SEARCH
+
+const inputSearch = document.getElementById("inputSearch");
+
+
+inputSearch.addEventListener("input", ()=>{
+    
+    movieContainer.innerHTML = ""
+
+    const filteredMovies = filteredMoviesTitle(movies, inputSearch.value.toLowerCase())
+    
+
+    const filteredMoviesGenres = filteredMoviesGenre(filteredMovies, selectContainer.value)
+
+    
+
+    printCards(filteredMoviesGenres, movieContainer)
+}) 
+
+function filteredMoviesTitle(moviesList, title) {   
+    if (title == 0){
+        return moviesList
+    }
+        return moviesList.filter((movie) => movie.title.toLowerCase().includes(title) );
+  
+ }
+
+
+
+// GET GENRES
+
+ function getGenres(moviesList){
+
+    const genres = []
+
+    let genre = moviesList.map(movie => movie.genres).flat()
+
+    genre.forEach(  genre =>{
+        if(!genres.includes(genre)){
+            genres.push(genre)
+        }
+    }) 
+    return genres
+       
+    }
+    getGenres(movies)
+
+
+// SELECT CONTENT   
+    
+
+    const selectContainer = document.getElementById("select")
+
+
+        function createOptions(genre) {
+
+            const options = document.createElement("option");
+        
+            options.innerHTML = `<option value="${genre}">${genre}</option>`
+
+            return options
+        }
+        
+
+    function printOptions(genresList, element) {
+        let fragment = document.createDocumentFragment();
+        
+        for (const genre of genresList) {
+            fragment.append( createOptions(genre)) ;
+             
+            }
+             element.appendChild(fragment);
+    }
+    printOptions(getGenres(movies), select)
+        
+//  FILTER GENRES
+
+    selectContainer.addEventListener("input", ()=>{
+
+        const filteredMoviesGenres = filteredMoviesGenre(movies, selectContainer.value)
+
+        
+        
+        const filteredMovies = filteredMoviesTitle(filteredMoviesGenres, inputSearch.value.toLowerCase())
+        
+        
+       movieContainer.innerHTML = ""
+    
+        printCards(filteredMovies, movieContainer)
+    }) 
+    
+    function filteredMoviesGenre(moviesList, genre) {
+        
+        if(genre == ""){
+            return moviesList
+        }
+        return moviesList.filter((movie) => movie.genres.includes(genre) );
+        
+     }
+    
