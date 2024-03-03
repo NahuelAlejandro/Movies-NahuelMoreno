2add792dbef326c58e42aa0e35dc5794d1dbff90
NahuelAlejandro <nahuelmoreno283@gamil.com>
2024-03-03 12:26:14 -0300

    se agrega la redireccion a la pag de favs

--- scripts/details.js
+++ scripts/details.js
@@ -1,47 +1,65 @@
 import{createArticle, createSpan, createTable, dropMenu}from "./module/functions.js"
 
-const openMenu = document.getElementById("menu");
-const drop = document.getElementById("dropMenu");
-const close = document.getElementById("close");
+let movies = []
+const url = "https://moviestack.onrender.com/api/movies"
+const init = {
+  method : "GET",
+  headers : {
+    'x-api-key' : "a15ddeac-fde6-4e38-84a7-084ba4dd7f04"
+  }
+}
+fetch(url, init)
+.then( response => response.json() )
+.then(data => {
+    movies = data.movies
+    
+
+    // MENU MOBILE
+    openMenu.addEventListener("click",()=>{
+        dropMenu(openMenu, drop, close)
+    });
+        
+    close.addEventListener("click", ()=>{
+        dropMenu(openMenu, drop, close)
+    });
+        
+    // Details Movie
+    const urlSearchParams = new URLSearchParams(location.search) 
+
+    const id = urlSearchParams.get("id");
+
+    const movie = movies.find(movie => movie.id == id);
+    
+    createArticle(movie, mainContent)
+
+    // Genre Tag
+    const gen = document.getElementById("genresContainer");
+    createSpan(movie.genres, gen)
+    
+    // Movie Table
+    createTable(movie, mainContent)
+})
+.catch(err => console.error(err))
 
 
 
- 
+    // MENU MOBILE
+    const openMenu = document.getElementById("menu");
+    const drop = document.getElementById("dropMenu");
+    const close = document.getElementById("close");
 
-openMenu.addEventListener("click",()=>{
-    dropMenu(openMenu, drop, close)
-});
-
-
-
-close.addEventListener("click", ()=>{
-    dropMenu(openMenu, drop, close)
-});
-
-// Details Movie
-const urlSearchParams = new URLSearchParams(location.search) 
-
-const id = urlSearchParams.get("id");
-
-
-const movie = movies.find(movie => movie.id == id);
-
-const mainContent = document.getElementById("mainContent");
+    // Details Movie
+    const mainContent = document.getElementById("mainContent");
 
 
 
-createArticle(movie, mainContent)
 
 
-const gen = document.getElementById("genresContainer");
-
-// Genre Tag
-createSpan(movie.genres, gen)
 
 
-// Movie Table
 
-createTable(movie, mainContent)
+
+
 
 
 
